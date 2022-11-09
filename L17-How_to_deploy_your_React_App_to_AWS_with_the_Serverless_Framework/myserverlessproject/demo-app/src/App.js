0d28ef06e04117761fc118bb98561955584b593a
Pavithradevadiga <pavithradevadiga1@gmail.com>
2022-11-09 23:17:16 +0530

    Deploy React app with AWS Serverless

---
+++ L17-How_to_deploy_your_React_App_to_AWS_with_the_Serverless_Framework/myserverlessproject/demo-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is a react app for completing code
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Pavithradevadiga/Serverless-with-AWS---Python-3.9"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Serverless
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
