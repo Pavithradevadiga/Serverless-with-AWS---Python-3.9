0bc24fc9b3f7a4816c4d343b2ee6aa57cc08702a
Pavithradevadiga <pavithradevadiga1@gmail.com>
2022-11-23 23:28:27 +0530

    Cognito-Amplify-React

---
+++ L25-Adding_CognitoSignup_&_Login_to_ReactAPP/Cognito-Amplify-React/amplify-react-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyreactapp74f67848": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
